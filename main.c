e19cfa8f6c54d23c322b8878ac70ca4c4ff7d908
georg_opk <georgkurmanov@gmail.com>
2026-04-23 01:32:34 +0300

    3

--- main.c
+++ main.c
@@ -4,8 +4,8 @@
 int main(int argc, char *argv[]) {
     Node *flight1 = NULL;
     TreeNode *flight2 = NULL;
-    //read_file(argv[1], &flight1);
-    read_file("flight_data14.bin", &flight1);
+    read_file(argv[1], &flight1);
+    //read_file("flight_data14.bin", &flight1);
     Node *current = flight1;
     while (current != NULL) {
         flight2 = appendTree(flight2, current->timestamp_ms, current->lon_rad, current->lat_rad, current->alt_m);
